cde0457592cb14e6dccc5c42fd98b273530134bb
edebigu <debora.iniguez@gmail.com>
2019-05-31 17:09:35 +0200

    v15: Nuevo test con overlaping y el minimo igual

--- src/test/java/tdd/intervalHierarchy/CloseIntervalOpenedInterval.java
+++ src/test/java/tdd/intervalHierarchy/CloseIntervalOpenedInterval.java
@@ -14,6 +14,13 @@ public class CloseIntervalOpenedInterval {
 		Interval another = new IntervalBuilder().min(1).max(7).kind(true).build();
 		assertTrue(one.isIntersected(another));
 	}
+	
+	@Test
+	public void testIsIntersectedOverlapingByLeftWithEqualsMin() {
+		Interval one = new IntervalBuilder().min(3).max(14).kind(true).build();
+		Interval another = new IntervalBuilder().min(3).max(7).kind(true).build();
+		assertTrue(one.isIntersected(another));
+	}
 
 
 }
