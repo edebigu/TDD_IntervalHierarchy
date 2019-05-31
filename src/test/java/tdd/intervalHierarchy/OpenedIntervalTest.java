520870201532c1a4ef0f91f7b9d8642d3c676851
edebigu <debora.iniguez@gmail.com>
2019-05-31 08:14:02 +0200

    v5: Nuevo test con overlaping por ambos lados
    El objeto intervalo que se compara (another) contiene al primero

--- src/test/java/tdd/intervalHierarchy/OpenedIntervalTest.java
+++ src/test/java/tdd/intervalHierarchy/OpenedIntervalTest.java
@@ -2,6 +2,7 @@ package tdd.intervalHierarchy;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+
 import tdd.intervalHierarchy.Builders.OpenedIntervalBuilder;
 
 
@@ -18,6 +19,13 @@ public class OpenedIntervalTest {
 	public void testIsIntersectedOverlapingByLeftWithEqualsMin() {
 		OpenedInterval one = new OpenedIntervalBuilder().min(3).max(14).build();
 		OpenedInterval another = new OpenedIntervalBuilder().min(3).max(7).build();
+		assertTrue(one.isIntersected(another));
+	}
+	
+	@Test
+	public void testIsIntersectedOverlapingBoth() {
+		OpenedInterval one = new OpenedIntervalBuilder().min(3).max(14).build();
+		OpenedInterval another = new OpenedIntervalBuilder().min(0).max(17).build();
 		assertTrue(one.isIntersected(another));
 	}
 	
