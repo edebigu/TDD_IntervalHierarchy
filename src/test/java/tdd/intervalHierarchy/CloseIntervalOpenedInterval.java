92aa2b9adbc4c7d168abece6704b997530329998
edebigu <debora.iniguez@gmail.com>
2019-05-31 17:07:54 +0200

    v14: nueva clase de test con un intevalo abierto y otro cerrado

---
+++ src/test/java/tdd/intervalHierarchy/CloseIntervalOpenedInterval.java
@@ -0,0 +1,19 @@
+package tdd.intervalHierarchy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import tdd.intervalHierarchy.Builders.IntervalBuilder;
+
+public class CloseIntervalOpenedInterval {
+	
+	@Test
+	public void testIsIntersectedOverlapingLeft() {
+		Interval one = new IntervalBuilder().min(3).max(14).kind(false).build();
+		Interval another = new IntervalBuilder().min(1).max(7).kind(true).build();
+		assertTrue(one.isIntersected(another));
+	}
+
+
+}
